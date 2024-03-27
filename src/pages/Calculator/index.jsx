61f0c7c474de0321baa96cc74cc696b0e695bff0
AliAbdelAall <3laych@gmail.com>
2024-03-27 20:09:13 +0200

    added calculator component

--- src/pages/Calculator/index.jsx
+++ src/pages/Calculator/index.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
-import Header from '../Header'
+import "../../styles/common/utilities.css"
+import "./style.css"
 
 const Calculator = () => {
   return (
-    <div>clac</div>
+    <div className='flex center clac_container'>
+      <div className='caculator-wrapper'>
+        <div className='flex column align-end space-between output'>
+          <div className='previous'> 12315*</div>
+          <div className='current white'>123123</div>
+        </div>
+        <button className='span-two'>AC</button>
+        <button>DEL</button>
+        <button>÷</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className='span-two'>=</button>
+      </div>
+    </div>
+
   )
 }
 
